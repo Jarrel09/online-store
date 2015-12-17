bccfa29577309c6c95d4f87b99850a606693cfb0
jarrel09 <jarrel@enversedesign.com>
2015-12-17 22:41:02 +0000

    Set up product scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+Product.delete_all
+# . . .
+Product.create!(title: "Apple iPhone 6 - Unlocked (Gold) ,16GB",
+                description:
+                %{<p>
+                    Phone 6. Bigger than bigger. iPhone 6 isn’t simply bigger - 
+                    it’s better in every way. Larger, yet dramatically thinner. 
+                    More powerful, but remarkably power efficient.
+                </p>},
+                image_url: 'iphone6.png',
+                price:      679.00)
+# . . .                
